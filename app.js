866345c8a29ba6dd8ee0abd7ad8d15dedba2b797
Andre Kolber <andrekolber1@gmail.com>
2021-09-20 15:53:44 -0400

    final changes commit

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ function addGiph(res) {
 	const numResults = res.data.length;
 	if (numResults) {
 		const randomGiphIdx = Math.floor(Math.random() * numResults);
-		const $newCol = $('<div>', { class: 'col-md-4 col-12 mb-4' });
+		const $newCol = $('<div>');
 		const $newGif = $('<img>', {
-			src   : res.data[randomGiphIdx].images.original.url,
-			class : 'giph',
+			src : res.data[randomGiphIdx].images.original.url,
 		});
 		$newCol.append($newGif);
 		$giphArea.append($newCol);
